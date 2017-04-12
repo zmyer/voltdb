50a69491a20c866f325bd8a9a3a573e2292faf1a
jcrawford <jcrawford@voltdb.com>
2017-04-12 02:11:28 -0400

    basic-update.sql, basic-delete.sql: added tests with (mostly scalar) sub-queries, to these DML tests.

--- tests/sqlcoverage/template/include/basic-delete.sql
+++ tests/sqlcoverage/template/include/basic-delete.sql
@@ -17,6 +17,7 @@
 -- {@comparableconstant = "42.42"}
 -- {@comparabletype = "numeric"}
 -- {@dmltable = "_table"}
+-- {@fromtables = "_table"}
 -- {@insertvals = "_id, _value[decimal], _value[decimal], _value[float]"}
 
 --SELECT
@@ -24,19 +25,67 @@
 -- Delete them all, then re-insert, then do trickier deletions
 -- test basic DELETE
 DELETE FROM @dmltable
+-- Confirm the values that were deleted
+SELECT @star FROM @dmltable
+
 INSERT INTO @dmltable VALUES (@insertvals)
 INSERT INTO @dmltable VALUES (@insertvals)
 INSERT INTO @dmltable VALUES (@insertvals)
 -- test where expressions
 --- test comparison ops (<, <=, =, >=, >)
 DELETE FROM @dmltable WHERE @columnpredicate
+-- Confirm the values that were deleted
+SELECT @star FROM @dmltable
+
 INSERT INTO @dmltable VALUES (@insertvals)
 INSERT INTO @dmltable VALUES (@insertvals)
 INSERT INTO @dmltable VALUES (@insertvals)
 --- test logic operators (AND) with comparison ops
 DELETE FROM @dmltable WHERE (_variable[@columntype] @cmp @comparableconstant) _logicop @columnpredicate
+-- Confirm the values that were deleted
+SELECT @star FROM @dmltable
+
 INSERT INTO @dmltable VALUES (@insertvals)
 INSERT INTO @dmltable VALUES (@insertvals)
 INSERT INTO @dmltable VALUES (@insertvals)
 --- test arithmetic operators (+, -, *, /) with comparison ops
 DELETE FROM @dmltable WHERE (_variable[@comparabletype] @aftermath) @cmp @comparableconstant
+-- Confirm the values that were deleted
+SELECT @star FROM @dmltable
+
+-- Test DML (DELETE) using sub-queries - in the WHERE clause (correlated and uncorrelated)
+-- (use of @somecmp rather than @cmp reduces the explosion of generated queries)
+INSERT INTO @dmltable VALUES (@insertvals)
+INSERT INTO @dmltable VALUES (@insertvals)
+INSERT INTO @dmltable VALUES (@insertvals)
+
+-- TODO: preferred version of DELETE statements, using table alias (does not work due to ENG-12295):
+--DELETE FROM @dmltable D10 WHERE __[#agg]      @somecmp (SELECT @agg(S._variable[#agg])                 FROM @fromtables S WHERE S._variable[#col] <= D10.__[#col])
+--INSERT INTO @dmltable VALUES (@insertvals)
+--DELETE FROM @dmltable D11 WHERE __[#agg]      @somecmp (SELECT @agg(S._variable[#agg])                 FROM @fromtables S)
+--INSERT INTO @dmltable VALUES (@insertvals)
+--DELETE FROM @dmltable D12 WHERE __[#agg]      <=       (SELECT @agg(S._variable[#agg @comparabletype]) FROM @fromtables S WHERE S.__[#agg] @somecmp @comparablevalue)
+--INSERT INTO @dmltable VALUES (@insertvals)
+--DELETE FROM @dmltable D13 WHERE __[#agg]      @somecmp (SELECT      S._variable[#agg]                  FROM @fromtables S ORDER BY @idcol _sortorder LIMIT 1)
+-- Deliberately (probably) invalid (LIMIT != 1):
+--INSERT INTO @dmltable VALUES (@insertvals)
+--DELETE FROM @dmltable D14 WHERE @updatecolumn @somecmp (SELECT      S.@updatecolumn                    FROM @fromtables S ORDER BY @idcol _sortorder LIMIT 2)
+--INSERT INTO @dmltable VALUES (@insertvals)
+--DELETE FROM @dmltable D15 WHERE @updatecolumn @somecmp (SELECT      S.@updatecolumn                    FROM @fromtables S ORDER BY @idcol _sortorder LIMIT 0)
+
+-- TODO: substitute version of DELETE statements, working around ENG-12295:
+DELETE FROM @dmltable[#tbl] WHERE __[#agg]      @somecmp (SELECT @agg(D10._variable[#agg])                 FROM @fromtables D10 WHERE D10._variable[#col] <= __[#tbl].__[#col])
+INSERT INTO @dmltable VALUES (@insertvals)
+DELETE FROM @dmltable       WHERE __[#agg]      @somecmp (SELECT @agg(D11._variable[#agg])                 FROM @fromtables D11)
+INSERT INTO @dmltable VALUES (@insertvals)
+DELETE FROM @dmltable       WHERE __[#agg]      <=       (SELECT @agg(D12._variable[#agg @comparabletype]) FROM @fromtables D12 WHERE D12.__[#agg] @somecmp @comparablevalue)
+INSERT INTO @dmltable VALUES (@insertvals)
+DELETE FROM @dmltable       WHERE __[#agg]      @somecmp (SELECT      D13._variable[#agg]                  FROM @fromtables D13 ORDER BY @idcol _sortorder LIMIT 1)
+-- Deliberately (probably) invalid (LIMIT != 1):
+INSERT INTO @dmltable VALUES (@insertvals)
+DELETE FROM @dmltable       WHERE @updatecolumn @somecmp (SELECT      D14.@updatecolumn                    FROM @fromtables D14 ORDER BY @idcol _sortorder LIMIT 2)
+INSERT INTO @dmltable VALUES (@insertvals)
+DELETE FROM @dmltable       WHERE @updatecolumn @somecmp (SELECT      D15.@updatecolumn                    FROM @fromtables D15 ORDER BY @idcol _sortorder LIMIT 0)
+
+-- Confirm the values that were deleted
+SELECT @star FROM @dmltable
--- tests/sqlcoverage/template/include/basic-update.sql
+++ tests/sqlcoverage/template/include/basic-update.sql
@@ -12,11 +12,14 @@
 -- Required preprocessor macros (with example values):
 -- {@aftermath = " _math _value[int:1,3]"}
 -- {@cmp = "_cmp"} -- all comparison operators (=, <>, !=, <, >, <=, >=)
+-- {@somecmp = "_somecmp"} -- a smaller list of comparison operators (=, <, >=)
 -- {@col_type = "decimal"}
 -- {@columnpredicate = "_numericcolumnpredicate"}
 -- {@comparableconstant = "42.42"}
 -- {@comparabletype = "numeric"}
 -- {@comparablevalue = "_numericvalue"}
+-- {@dmltable = "_table"}
+-- {@fromtables = "_table"}
 -- {@updatecolumn = "CASH"}
 -- {@updatesource = "ID"}
 -- {@updatevalue = "_value[decimal]"}
@@ -31,26 +34,64 @@
 -- Also, to avoid updating the partition key (by convention, "ID")
 
 -- test simple update
-UPDATE _table SET @updatecolumn = @updatevalue
+UPDATE @dmltable SET @updatecolumn = @updatevalue
 --- test comparison ops (<, <=, =, >=, >)
-UPDATE _table SET @updatecolumn = @updatevalue WHERE @columnpredicate
+UPDATE @dmltable SET @updatecolumn = @updatevalue WHERE @columnpredicate
+-- Confirm the values that were updated
+SELECT @star FROM @dmltable
+
 --- test logic operators (AND) with comparison ops
-UPDATE _table SET @updatecolumn = @updatevalue WHERE (@updatecolumn @cmp @comparablevalue) _logicop @columnpredicate
+UPDATE @dmltable SET @updatecolumn = @updatevalue WHERE (@updatecolumn @cmp @comparablevalue) _logicop @columnpredicate
 --- test arithmetic operators (+, -, *, /) with comparison ops
-UPDATE _table SET @updatecolumn = @updatevalue WHERE (_variable[@comparabletype] @aftermath) @cmp @comparableconstant
+UPDATE @dmltable SET @updatecolumn = @updatevalue WHERE (_variable[@comparabletype] @aftermath) @cmp @comparableconstant
+-- Confirm the values that were updated
+SELECT @star FROM @dmltable
+
 -- test set expression
+UPDATE @dmltable SET @updatecolumn = @updatesource @aftermath
+-- Confirm the values that were updated
+SELECT @star FROM @dmltable
+
 --- test arithmetic (+, -, *, /) ops
-UPDATE _table SET @updatecolumn = @updatesource @aftermath
--- test type casting
 -- These fail with an odd message and kill the volt run.
---UPDATE _table SET @updatecolumn = _variable _math _value[byte]
---UPDATE _table SET @updatecolumn = _variable _math _value[int16]
---UPDATE _table SET @updatecolumn = _variable _math _value[int32]
---UPDATE _table SET @updatecolumn = _variable _math _value[int64]
---UPDATE _table SET @updatecolumn = _variable _math _value[float]
---UPDATE _table SET @updatecolumn = _variable _math _value[string]
---UPDATE _table SET @updatecolumn = _variable _math _value[decimal]
+--UPDATE @dmltable SET @updatecolumn = _variable _math _value[byte]
+--UPDATE @dmltable SET @updatecolumn = _variable _math _value[int16]
+--UPDATE @dmltable SET @updatecolumn = _variable _math _value[int32]
+--UPDATE @dmltable SET @updatecolumn = _variable _math _value[int64]
+--UPDATE @dmltable SET @updatecolumn = _variable _math _value[float]
+--UPDATE @dmltable SET @updatecolumn = _variable _math _value[string]
+--UPDATE @dmltable SET @updatecolumn = _variable _math _value[decimal]
+-- test type casting
 -- ENG-495 test type casting errors
 -- These don't make it out of the planner
---UPDATE _table SET @updatecolumn = _value[string]
---UPDATE _table SET @updatecolumn = _value[decimal]
+--UPDATE @dmltable SET @updatecolumn = _value[string]
+--UPDATE @dmltable SET @updatecolumn = _value[decimal]
+-- Confirm the values that were updated
+--SELECT @star FROM @dmltable
+
+-- Test DML (UPDATE) using sub-queries
+-- ... in the WHERE clause (correlated and uncorrelated)
+-- (use of @somecmp rather than @cmp reduces the explosion of generated queries)
+UPDATE @dmltable U10 SET @updatecolumn = @updatevalue WHERE __[#agg]      @somecmp (SELECT @agg(S._variable[#agg])                 FROM @fromtables S WHERE S._variable[#col] <= U10.__[#col])
+UPDATE @dmltable U11 SET @updatecolumn = @updatevalue WHERE __[#agg]      @somecmp (SELECT @agg(S._variable[#agg])                 FROM @fromtables S)
+UPDATE @dmltable U12 SET @updatecolumn = @updatevalue WHERE __[#agg]      <=       (SELECT @agg(S._variable[#agg @comparabletype]) FROM @fromtables S WHERE S.__[#agg] @somecmp @comparablevalue)
+UPDATE @dmltable U13 SET @updatecolumn = @updatevalue WHERE __[#agg]      @somecmp (SELECT      S._variable[#agg]                  FROM @fromtables S ORDER BY @idcol _sortorder LIMIT 1)
+-- Deliberately (probably) invalid (LIMIT != 1):
+UPDATE @dmltable U14 SET @updatecolumn = @updatevalue WHERE @updatecolumn @somecmp (SELECT      S.@updatecolumn                    FROM @fromtables S ORDER BY @idcol _sortorder LIMIT 2)
+UPDATE @dmltable U15 SET @updatecolumn = @updatevalue WHERE @updatecolumn @somecmp (SELECT      S.@updatecolumn                    FROM @fromtables S ORDER BY @idcol _sortorder LIMIT 0)
+-- Confirm the values that were updated
+SELECT @star FROM @dmltable
+
+-- ... in the SET clause (correlated and uncorrelated)
+UPDATE @dmltable U16 SET @updatecolumn = (SELECT @agg(S._variable[#agg @comparabletype]) FROM @fromtables S WHERE S._variable[#col] <= U16.__[#col])
+UPDATE @dmltable U17 SET @updatecolumn = (SELECT @agg(S._variable[#agg @comparabletype]) FROM @fromtables S)
+UPDATE @dmltable U18 SET @updatecolumn = (SELECT @agg(S._variable[#agg @comparabletype]) FROM @fromtables S WHERE S.__[#agg] @somecmp @comparablevalue)
+UPDATE @dmltable U19 SET @updatecolumn = (SELECT      S._variable[#agg @comparabletype]  FROM @fromtables S ORDER BY @idcol _sortorder LIMIT 1)
+-- Deliberately (probably) invalid (LIMIT != 1):
+UPDATE @dmltable U20 SET @updatecolumn = (SELECT      S.@updatecolumn                    FROM @fromtables S ORDER BY @idcol _sortorder LIMIT 2)
+UPDATE @dmltable U21 SET @updatecolumn = (SELECT      S.@updatecolumn                    FROM @fromtables S ORDER BY @idcol _sortorder LIMIT 0)
+-- Confirm the values that were updated
+SELECT @star FROM @dmltable
+
+-- TODO: ???
+-- ... in both the SET and WHERE clauses (correlated and uncorrelated)
