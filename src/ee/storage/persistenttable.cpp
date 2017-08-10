0bd8a6da8637915ec8af7f3f52455a70ae0c2acc
dhan <dhan@voltdb.com>
2017-08-10 12:32:05 -0400

    never reset flag

--- src/ee/storage/persistenttable.cpp
+++ src/ee/storage/persistenttable.cpp
@@ -1082,7 +1082,7 @@ void PersistentTable::updateTupleWithSpecificIndexes(TableTuple& targetTupleToUp
     // reset conflict flag
     if (hasDRTimestampColumn() && updateDRTimestamp) {
         // remote cluster ID
-        int16_t clusterIdFromTuple = ExecutorContext::getClusterIdFromHiddenNValue(sourceTupleWithNewValues.getHiddenNValue(m_drTimestampColumnIndex));
+//        int16_t clusterIdFromTuple = ExecutorContext::getClusterIdFromHiddenNValue(sourceTupleWithNewValues.getHiddenNValue(m_drTimestampColumnIndex));
 //        if (clusterIdFromTuple - clusterId == 0) {
 //            ExecutorContext::resetConflictFlagFromHiddenNValue(&sourceTupleWithNewValues, m_drTimestampColumnIndex);
 //        }
