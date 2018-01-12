e5cdcf289b4faf62078b3e30908e88caa6569b11
John Hugg <jhugg@voltdb.com>
2018-01-12 12:41:36 -0500

    WIP commit for low impact delete sysproc work. This was mostly written before more final design work, finished all-partition NT work, and ylu's work on the partitioned proc.

--- src/frontend/org/voltdb/SystemProcedureCatalog.java
+++ src/frontend/org/voltdb/SystemProcedureCatalog.java
@@ -215,6 +215,7 @@ public class SystemProcedureCatalog {
                                                                                                            false, false, false, 0,    VoltType.INVALID,   true,  false, true,  true,      false,  false,            false,        false ));
         builder.put("@StartNodeDRConsumerNT",   new Config("org.voltdb.sysprocs.RestartDRConsumerNT$StartNodeDRConsumerNT",
                                                                                                            false, false, false, 0,    VoltType.INVALID,   true,  false, true,  true,      false,  false,            false,        false ));
+        builder.put("@LowImpactDelete",         new Config("org.voltdb.sysprocs.LowImpactDelete",          true,  false, false, 0,    VoltType.INVALID,   false, false, false, true,      false,  false,            false,        false ));
         listing = builder.build();
     }
 }
